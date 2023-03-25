d1bb89958ead2e2cef33574abd0f4f27e560226a
DanielOS96 <39861177+DanielOS96@users.noreply.github.com>
2023-03-25 07:28:48 +0000

    Comments

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,14 +13,4 @@ public class GameManager : MonoBehaviour
         score += value;
         Debug.Log("Score: " + score);
     }
-
-    // The 'gameObject' property inherited from MonoBehaviour is an example of a reference
-    // type variable. Reference type variables store a reference to the actual data on the
-    // heap. When reference type variables are passed to methods or assigned to other
-    // reference type variables, they still refer to the same data on the heap. Changes
-    // made to the data through one reference are visible to other references as well.
-    //
-    // In this script, the 'gameObject' property is used implicitly when accessing the
-    // 'transform' and 'GetComponent()' methods. These methods, in turn, access data on
-    // the heap through the reference stored in the 'gameObject' property.
 }
